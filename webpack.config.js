abd14b1e28e1c443e95acaed18fdea5686ceb14d
Malcolm Diller <dillerm@oregonstate.edu>
2017-12-03 20:17:40 -0800

    added option to specify build directory

--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,32 @@ const path = require("path");
 
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
-config = {
-	context: __dirname,
-	entry: path.resolve(__dirname, "src"),
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				loader: "babel-loader"
-			},
-			{
-				test: /\.json$/,
-				loader: "json-loader"
-			}
+module.exports = function(env) {
+	var build_dir = env && env.build_dir ? env.build_dir : path.resolve(__dirname, "build");
+	return {
+		context: __dirname,
+		entry: path.resolve(__dirname, "src"),
+		module: {
+			loaders: [
+				{
+					test: /\.js$/,
+					exclude: /(node_modules)/,
+					loader: "babel-loader"
+				},
+				{
+					test: /\.json$/,
+					loader: "json-loader"
+				}
+			]
+		},
+		output: {
+			filename: "bundle.js",
+			path: build_dir
+		},
+		plugins: [
+			new CopyWebpackPlugin([
+				{ from: 'public' }
+			])
 		]
-	},
-	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "build"),
-		publicPath: "build/"
-	},
-	plugins: [
-		new CopyWebpackPlugin([
-			{ from: 'public' }
-		])
-	]
+	};
 };
-
-module.exports = config;
